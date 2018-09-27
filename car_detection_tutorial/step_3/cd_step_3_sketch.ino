1528262e92c054241cbe25d824f65c25206ded3e
trexdog <38870270+trexdog@users.noreply.github.com>
2018-09-27 10:24:10 -0600

    Merge changes from generic tutorials
    Changes include:
    - Fix for potential crashes when batch size -n > 1
    - Make code and CMake more portable (i.e. compile on Windows with MS Visual Studio)

--- car_detection_tutorial/step_3/cd_step_3_sketch.ino
+++ car_detection_tutorial/step_3/cd_step_3_sketch.ino
@@ -257,7 +257,7 @@ struct VehicleDetection : BaseDetection{
         return netReader.getNetwork();
     }
 
-    void fetchResults() {
+    void fetchResults(int inputBatchSize) {
         if (!enabled()) return;
         results.clear();
         if (resultsFetched) return;
@@ -279,7 +279,7 @@ struct VehicleDetection : BaseDetection{
 			r.location.width = detections[proposalOffset + 5] * width - r.location.x;
 			r.location.height = detections[proposalOffset + 6] * height - r.location.y;
 
-			if ((image_id < 0) || (image_id >= maxBatch)) {  // indicates end of detections
+			if ((image_id < 0) || (image_id >= inputBatchSize)) {  // indicates end of detections
 				break;
 			}
 			if (PARAMETERS_r) {
@@ -559,7 +559,7 @@ int main_function() {
 
         // read input (video) frames, need to keep multiple frames stored for batching
         const int maxNumInputFrames = VehicleDetection.maxBatch + 1;  // +1 to avoid overwrite
-        cv::Mat inputFrames[maxNumInputFrames];
+        cv::Mat* inputFrames = new cv::Mat[maxNumInputFrames];
         std::queue<cv::Mat*> inputFramePtrs;
         for(int fi = 0; fi < maxNumInputFrames; fi++) {
         	inputFramePtrs.push(&inputFrames[fi]);
@@ -601,7 +601,7 @@ int main_function() {
 
 			/* *** Pipeline Stage 0: Prepare and Infer a Batch of Frames *** */
         	// if there are more frames to do then prepare and start batch
-			if (haveMoreFrames) {
+			if (haveMoreFrames && (inputFramePtrs.size() >= VehicleDetection.maxBatch)) {
 				// prepare a batch of frames
         		FramePipelineFifoItem ps0s1i;
 				for(numFrames = 0; numFrames < VehicleDetection.maxBatch; numFrames++) {
@@ -646,7 +646,7 @@ int main_function() {
 					detection_time = std::chrono::duration_cast<ms>(t1 - t0);
 
 					// parse inference results internally (e.g. apply a threshold, etc)
-					VehicleDetection.fetchResults();
+					VehicleDetection.fetchResults(ps0s1i.batchOfInputFrames.size());
 
 					// prepare a FramePipelineFifoItem for each batched frame to get its detection results
 					for (auto && bFrame : ps0s1i.batchOfInputFrames) {
@@ -870,6 +870,8 @@ int main_function() {
         	VehicleDetection.printPerformanceCounts();
         	VehicleAttribs.printPerformanceCounts();
         }
+
+		delete [] inputFrames;
     }
     catch (const std::exception& error) {
         slog::err << error.what() << slog::endl;
